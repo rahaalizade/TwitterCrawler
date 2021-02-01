a06163ccd08ca2a071e281cf185ec2564f500858
Fateme Alizade <Alizadeh.fatemeh@yahoo.com>
2021-02-02 00:33:31 +0330

    Remove logs

--- consumer.py
+++ consumer.py
@@ -1,16 +1,11 @@
 from kafka import KafkaConsumer
-from pymongo import MongoClient
 from json import loads
 import logging
 
-
-logging.basicConfig(level=logging.ERROR)
-logging.info("meh")
+logging.basicConfig(level=logging.DEBUG)
 bootstrap_servers = 'localhost:9092'
 consumer = KafkaConsumer(bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest')
 consumer.subscribe(topics=['rawtweets'])
-logging.info('this shit was created')
 
 for tweet in consumer:
-    logging.info('whats wrong?T_T')
     print(tweet.value.decode('utf-8'))
